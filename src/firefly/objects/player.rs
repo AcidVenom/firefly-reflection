13cef4684f5025e3273be2498577fde2b7effc72
Daniel Konings <konings_304@hotmail.com>
2019-08-22 16:34:26 +0200

    - One hoppy boi

--- src/firefly/objects/player.rs
+++ src/firefly/objects/player.rs
@@ -6,7 +6,8 @@ pub struct Player {
     texture: gfx::Texture2D,
     transform: core::Transform,
     velocity: nalgebra_glm::Vec2,
-    grounded: bool
+    grounded: bool,
+    squish_timer: f32
 }
 
 impl objects::GameObject for Player {
@@ -24,10 +25,14 @@ impl Player {
             texture: gfx::Texture2D::from_image(display, "assets/textures/characters/player.png"),
             transform: core::Transform::new(),
             velocity: nalgebra_glm::vec2(0.0, 0.0),
-            grounded: false
+            grounded: false,
+            squish_timer: 1.0
         };
 
-        player.transform.set_size_2d(&player.texture.dimensions_f());
+        player.transform
+            .set_size_2d(&player.texture.dimensions_f())
+            .set_translation_2d_f(0.0, -300.0)
+            .set_anchor_2d_f(0.0, 0.25);
 
         player
     }
@@ -40,29 +45,89 @@ impl Player {
         &mut self.transform
     }
 
+    fn update_animations(&mut self, dt: f32) {
+
+        // Variables
+        let pi = 3.14159;
+        let squish_scale = 0.175;
+        let squish_duration = 0.25;
+
+        // Flipping
+        let mut speed_scale = 1.0;
+        if self.velocity.x != 0.0 {
+            speed_scale = self.velocity.x.abs() / self.velocity.x;
+        }
+        
+        // Hopping
+        let hop_ratio = self.velocity.y.max(0.0).min(300.0) / 300.0;
+        let hop_scale = 1.0 + (hop_ratio * pi * 2.0).sin() * 0.125;
+        let hop_rot = (hop_ratio * pi).sin() * -0.25;
+
+        // Squishing
+        if self.squish_timer < 1.0 {
+            self.squish_timer += dt * (1.0 / squish_duration);
+            self.squish_timer = self.squish_timer.min(1.0);
+        }
+
+        let hop_squish = (self.squish_timer * pi).sin() * squish_scale;
+
+        // Apply
+        self.transform
+            .set_scale_2d_f(speed_scale * (1.0 + hop_squish), hop_scale - hop_squish * 0.5)
+            .set_orientation(hop_rot);
+    }
+
     pub fn update(&mut self, dt: f32, window: &core::Window) {
 
         let fall_speed = 1000.0;
-        let jump_force = 500.0;
         let border = -300.0;
+        let damping = 50.0;
+        let acceleration = 1500.0;
+        let max_speed = 300.0;
+        let max_fall_speed = 2000.0;
+        let hop_force = 300.0;
 
         let mut t = self.transform.translation_2d() + self.velocity * dt;
 
-        if window.is_key_down(glium::glutin::VirtualKeyCode::Space) && self.grounded {
-            self.velocity.y = jump_force;
-            self.grounded = false;
-        }
-
-        if t.y > border {
-            self.velocity.y -= dt * fall_speed;
-        }
-
-        if t.y < border {
+        if t.y <= border && !self.grounded {
             t.y = border;
             self.velocity.y = 0.0;
             self.grounded = true;
+            self.squish_timer = 0.0;
         }
 
+        if self.grounded {
+
+            self.velocity.x = nalgebra_glm::lerp_scalar(self.velocity.x, 0.0, 1.0 - f32::powf(1.0 / damping, dt));
+
+            if window.is_key_down(glium::glutin::VirtualKeyCode::Right) {
+                if self.velocity.x < 0.0 {
+                    self.velocity.x *= 0.75;
+                }
+                self.velocity.x += acceleration;
+                self.velocity.y = hop_force;
+                self.grounded = false;
+            }
+            else if window.is_key_down(glium::glutin::VirtualKeyCode::Left) {
+                if self.velocity.x > 0.0 {
+                    self.velocity.x *= 0.75;
+                }
+                self.velocity.x -= acceleration;
+                self.velocity.y = hop_force;
+                self.grounded = false;
+            }
+        }
+        else {
+            self.velocity.y -= dt * fall_speed;
+        }
+
+        self.velocity = nalgebra_glm::clamp_vec(
+            &self.velocity, 
+            &nalgebra_glm::vec2(-max_speed, -max_fall_speed), 
+            &nalgebra_glm::vec2(max_speed, hop_force));
+        
         self.transform.set_translation_2d(&t);
+
+        self.update_animations(dt);
     }
 }
